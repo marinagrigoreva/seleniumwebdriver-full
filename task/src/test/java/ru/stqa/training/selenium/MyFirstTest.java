0e6e5f790f0d37af0fed256c734c4a9bcc0ce4f6
Marina Grigoryeva <grigoreva-mar@yandex.ru>
2017-04-18 23:58:32 +0400

    Задание 3

---
+++ task/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -0,0 +1,41 @@
+package ru.stqa.training.selenium;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+
+
+/**
+ * Created by Марина on 18.04.2017.
+ */
+public class MyFirstTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start(){
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver,30);
+
+    }
+
+    @Test
+    public void MyFirstTest(){
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("remember_me")).click();
+        driver.findElement(By.xpath("//button[@name='login']")).click();
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+}
