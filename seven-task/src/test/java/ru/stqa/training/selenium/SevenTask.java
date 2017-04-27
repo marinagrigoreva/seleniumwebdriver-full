d5dc6c030159b49e78c5df6d3c0d1be378202d1c
Marina Grigoryeva <grigoreva-mar@yandex.ru>
2017-04-27 10:21:15 +0400

    Задание 7:вынос методов в отдельный класс

--- seven-task/src/test/java/ru/stqa/training/selenium/SevenTask.java
+++ seven-task/src/test/java/ru/stqa/training/selenium/SevenTask.java
@@ -1,48 +1,16 @@
 package ru.stqa.training.selenium;
-
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 import static org.junit.Assert.assertTrue;
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 import static junit.framework.TestCase.assertFalse;
 
 /**
  * Created by Марина on 27.04.2017.
  */
-public class SevenTask {
-    private WebDriver driver;
-    private WebDriverWait wait;
+public class SevenTask extends TestBase{
 
-    @Before
-    public void start(){
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        wait = new WebDriverWait(driver,30);
-    }
 
-    public boolean isElementPresent(By locator){
-        try {
-            driver.findElement(locator);
-            return true;
-        } catch (InvalidSelectorException ex){
-            throw ex;
-        } catch (NoSuchElementException ex){
-            return false;
-        }
-    }
-
-    public boolean areElementsPresent(By locator){
-        return driver.findElements(locator).size()>0;
-    }
 
     @Test
     public void HeaderPresent(){
@@ -56,7 +24,7 @@ public class SevenTask {
         int i=1;
         while (i<=length){
             driver.findElement(By.xpath("//li[@id='app-']["+i+"]/a/span[@class='name']")).click(); //Кликнуть по пункту меню номер i
-            assertFalse(isElementPresent(By.xpath("//h1"))); //Проверить что есть заголовок h1
+            assertTrue(isElementPresent(By.xpath("//h1"))); //Проверить что есть заголовок h1
             List<WebElement> submenu= driver.findElements(By.xpath("//li[@id='app-']/ul/li[not(contains(@class, 'selected'))]/a/span[@class='name']")); // Найдем подпункты меню
             int sublength = submenu.size();  //Узнаем количество подпунктов меню
             if (sublength>0){  //Если количество подпунктов больше 0, то
@@ -72,9 +40,5 @@ public class SevenTask {
         }
     }
 
-    @After
-    public void stop(){
-        driver.quit();
-        driver = null;
-    }
+
 }
---
+++ seven-task/src/test/java/ru/stqa/training/selenium/TestBase.java
@@ -0,0 +1,49 @@
+package ru.stqa.training.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.InvalidSelectorException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Марина on 27.04.2017.
+ */
+public class TestBase {
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    @Before
+    public void start(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver,30);
+    }
+
+    public boolean isElementPresent(By locator){
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (InvalidSelectorException ex){
+            throw ex;
+        } catch (NoSuchElementException ex){
+            return false;
+        }
+    }
+
+    public boolean areElementsPresent(By locator){
+        return driver.findElements(locator).size()>0;
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+
+}
